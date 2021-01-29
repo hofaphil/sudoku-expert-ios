124276167366356720983e7219adbef566a50ecd
Philipp Hofer <philipp.hofer@aol.com>
2021-01-29 16:15:30 +0100

    finished settings with toggle switch state saving

---
+++ Sudoku/Gui/Settings/SettingsButton.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsButton.swift
+//  Sudoku
+//
+//  Created by Philipp Hofer on 29.01.21.
+//  Copyright © 2021 Philipp Hofer. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsButton()
+    }
+}
